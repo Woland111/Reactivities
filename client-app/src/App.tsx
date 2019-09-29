b9dbf89810074e0acf161249cae318f4b16aef30
Woland111 <Woland111@gmail.com>
2019-09-29 13:21:19 +0200

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Header, Icon, List, ListItem } from "semantic-ui-react";
 import './App.css';
+import axios from "axios";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editt <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+componentDidMount() {
+  axios.get("http://localhost:5000/api/values")
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      })
+    })
+}
+
+  render() {
+    return (
+      <div>
+
+          <Header as='h2' icon>
+            <Icon name='users' />
+            <Header.Content>
+              Reactivities
+            </Header.Content>
+          </Header>
+          <List>
+          {this.state.values.map((value: any) => { return (
+            <ListItem key={value.id}>{value.name}</ListItem>
+          )})}
+          </List>
+      </div>
+    );
+  }
 }
 
 export default App;
