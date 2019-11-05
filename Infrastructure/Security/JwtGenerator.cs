0b469270934d13b8dcfc8fed1a0ae31896b377ce
Woland111 <Woland111@gmail.com>
2019-11-05 09:58:09 +0100

    Section 12 - 05.11 - 2

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,5 +1,12 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 namespace Infrastructure.Security
 {
@@ -7,7 +14,21 @@ namespace Infrastructure.Security
     {
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my super secret key"));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
