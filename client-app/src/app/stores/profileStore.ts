ad42bffc65a0bc5e074b720b29f6cec587ee6465
Luka <Woland111@gmail.com>
2019-11-30 22:29:03 +0100

    Section 17 - 30.11 - 2

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,8 +1,31 @@
 import { RootStore } from "./rootStore";
+import { observable, runInAction, action } from "mobx";
+import { IProfile } from "../models/profile";
+import agent from "../api/agent";
+import { thisExpression } from "@babel/types";
 
 export default class ProfileStore {
   rootStore: RootStore;
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
+  }
+
+  @observable profile: IProfile | null = null;
+  @observable loadingProfile = true;
+
+  @action loadProfile = async (username: string) => {
+    this.loadingProfile = true;
+    try {
+      var profile = await agent.Profiles.get(username);
+      runInAction(() => {
+        this.profile = profile;
+      })
+    } catch (error) {
+      console.log(error);
+    } finally {
+      runInAction(() => {
+        this.loadingProfile = false;
+      })
+    }
   }
 }
--- client-app/src/features/profile/ProfileContent.tsx
+++ client-app/src/features/profile/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
+import { ProfilePhotos } from './ProfilePhotos'
 
 const panes = [
     {menuItem: 'About', render: () => <Tab.Pane>This is about</Tab.Pane>},
-    {menuItem: 'Photos', render: () => <Tab.Pane>This is photos</Tab.Pane>},
+    {menuItem: 'Photos', render: () => <ProfilePhotos />},
     {menuItem: 'Activities', render: () => <Tab.Pane>This is activities</Tab.Pane>},
     {menuItem: 'Followers', render: () => <Tab.Pane>This is followers</Tab.Pane>},
     {menuItem: 'Following', render: () => <Tab.Pane>This is following</Tab.Pane>}
