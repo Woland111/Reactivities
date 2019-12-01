3f6d28db965b253c44a1b25d1abae09baecb761a
Luka <Woland111@gmail.com>
2019-12-01 15:14:38 +0100

    01.12 -1

---
+++ client-app/src/app/common/photoUploadWidget/PhotoWidgetDropzone.tsx
@@ -0,0 +1,39 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Icon, Header } from 'semantic-ui-react'
+
+const dropzoneStyle = {
+    border: 'dashed 3px',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    height: '200px',
+    paddingTop: '30px',
+    textAlign: 'center' as 'center'
+}
+
+const dropzoneActive = {
+    borderColor: 'green'
+}
+
+interface IProps {
+    setFiles: (files: object[]) => void
+}
+
+const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
+  const onDrop = useCallback(acceptedFiles => {
+    setFiles(acceptedFiles.map((file: object) => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+    })))
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()} style={isDragActive ? {...dropzoneStyle, ...dropzoneActive} : dropzoneStyle} >
+      <input {...getInputProps()} />
+      <Icon name="upload" size="huge" />
+      <Header content="Drag image here" />
+    </div>
+  )
+}
+
+export default PhotoWidgetDropzone
--- client-app/src/features/profile/ProfilePhotos.tsx
+++ client-app/src/features/profile/ProfilePhotos.tsx
@@ -1,20 +1,49 @@
-import React, { useContext } from 'react'
-import { Card, Header, Tab, Image } from 'semantic-ui-react'
-import { RootStoreContext } from '../../app/stores/rootStore'
+import React, { useContext, useState } from "react";
+import { Card, Header, Tab, Image, Button, Grid } from "semantic-ui-react";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import PhotoUploadWidget from "../../app/common/photoUploadWidget/PhotoUploadWidget";
 
 export const ProfilePhotos = () => {
-    const rootStore = useContext(RootStoreContext);
-    const { profile } = rootStore.profileStore;
-    return (
-        <Tab.Pane>
-            <Header icon="image">Photos</Header>
+  const rootStore = useContext(RootStoreContext);
+  const { profile, isCurrentUser } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(true);
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16} style={{ paddingBotton: 0 }}>
+          <Header icon="image" floated="left">
+            Photos
+          </Header>
+          {isCurrentUser && (
+            <Button
+              basic
+              floated="right"
+              content={addPhotoMode ? "Cancel" : "Add Photo"}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget />
+          ) : (
             <Card.Group>
-                {profile && profile.photos.map(photo => (
-                    <Card key={photo.id}>
-                        <Image src={photo.url} />
-                    </Card>
+              {profile &&
+                profile.photos.map(photo => (
+                  <Card key={photo.id}>
+                    <Image src={photo.url} />
+                    {isCurrentUser && (
+                      <Button.Group fluid widths={2}>
+                        <Button basic positive content="Main" />
+                        <Button basic negative icon="trash" />
+                      </Button.Group>
+                    )}
+                  </Card>
                 ))}
             </Card.Group>
-        </Tab.Pane>
-    )
-}
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
